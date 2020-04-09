dcddc65a8de58584d852c07cd07e9fe21c9c4f0c
David G <david.glzcr@gmail.com>
2020-04-09 03:00:50 -0500

    project start

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+// routes
+
+router.get('/', (req, res) => {
+    res.render('index.html', { title: 'First website' });
+});
+
+
+router.get('/contact', (req, res) => {
+    res.render('contact.html', { title: 'Contact page' });
+});
+
+
+module.exports = router;
